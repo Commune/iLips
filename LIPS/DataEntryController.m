94ec42083f6cde67062758df69e2ebccd7a1ae42
Trevor <trevor.narayan@gmail.com>
2011-03-28 02:28:34 -0400

    recommitng dataentrycontroller because it was apparently out of date

--- LIPS/DataEntryController.m
+++ LIPS/DataEntryController.m
@@ -77,6 +77,8 @@
 	NSString *shockResponse = shockSwitch.on?@"Yes":@"No";
 	[DecisionFetcher assignResponse:@"Shock" withValue:shockResponse];
 	
+	[thePatient printSelf];
+	
     [self.navigationController pushViewController:[[ScoreViewController alloc] initWithScore:total] animated:YES];
 }
 
