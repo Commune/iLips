64580ebbe460b7a363ecc0eb304514a8360833b7
Trevor <trevor.narayan@gmail.com>
2011-04-18 17:32:53 -0400

    added password view and checklist view, filling in content now

---
+++ ChecklistViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChecklistViewController.h
+//  LIPS
+//
+//  Created by Duke Student on 4/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ChecklistViewController : UIViewController {
+    
+}
+
+@end
